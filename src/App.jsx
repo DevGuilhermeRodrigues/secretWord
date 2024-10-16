5ceecb7b2883c61b3d41ccd7e08a212d9478e54c
DevGuilhermeRodrigues <guilhermegrillo25@gmail.com>
2024-10-16 20:52:37 -0300

    Component StarScreen

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+// CSS
+import StartScreen from './components/StartScreen/StartScreen'
+import './style.css' 
+
+function App() {
+
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StartScreen/StartScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './StartScreen.css'
+
+function StartScreen() {
+  return (
+    <div>
+        <h1>Start Screen</h1>
+        <p>Clique no botão abaixo para começar a jogar!</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
